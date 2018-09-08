fa65cd01cb103447dece6082785eab909ae91016
zpei100 <zhengqing.pei@gmail.com>
2018-09-08 10:57:06 -0400

    added comments

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -3,6 +3,8 @@ import { Field, reduxForm } from 'redux-form';
 
 class NewPost extends Component {
   renderForm(field) {
+    //research {...field.input}
+    //specifically look at the field.input array/object and look at each of the event handler definitions
     return (
       <div>
         Hello
@@ -17,11 +19,17 @@ class NewPost extends Component {
   }
 
   render() {
+    //handleSubmit is a property passed to this component via the link at the bottom
+    //check out what handleSubmit does exactly and how it interacts with redux-form
     return (
       <form onSubmit={this.props.handleSubmit(this.formSubmit.bind(this))}>
         <Field label="Title" name="title" component={this.renderForm} />
 
-        <Field label="Categories" name="Categories" component={this.renderForm} />
+        <Field
+          label="Categories"
+          name="Categories"
+          component={this.renderForm}
+        />
 
         <Field label="Content" name="Content" component={this.renderForm} />
         <button type="submit">Post!!!</button>
@@ -31,9 +39,13 @@ class NewPost extends Component {
 }
 
 function validate() {
-  return {}
+  //implement details on error object
+
+  return {};
 }
 
+//how does validate works behind the scene?
+//what does the form property do?
 
 export default reduxForm({
   validate: validate,
